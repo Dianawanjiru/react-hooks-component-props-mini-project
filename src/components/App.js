d211f37fe7b1f9d2955f7ba8db7bb3d39d430889
Diana <diana.wanjiru@student.moringaschool.com>
2022-09-23 10:02:50 +0300

    completed test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 import blogData from "../data/blog";
-
 console.log(blogData);
 
 function App() {
+  const posts = blogData.posts;
+  
   return (
     <div className="App">
+      <Header header={blogData.name}/>
+      <About src={blogData.image}/>
+      {posts.map((post)=>{
+        return (
+          <ArticleList 
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}/>
+        )
+      })}
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
